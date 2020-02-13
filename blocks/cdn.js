5ac5b0e75c9c95a3f52f4c3c0abd17aeecca42a8
stel-la <59512425+stel-la@users.noreply.github.com>
2020-02-13 17:45:02 +0000

    Add files via upload

--- blocks/cdn.js
+++ blocks/cdn.js
@@ -1,10 +1,17 @@
 function checkBlock(queryw){
 const Http = new XMLHttpRequest();
-const url = "https://en.wikipedia.org/w/api.php?action=query&prop=extracts&exsentences=1&exlimit=1&titles=" + queryw + "&explaintext=1&formatversion=2";
+const url = "https://en.wikipedia.org/w/api.php?action=query&prop=extracts&exsentences=1&exlimit=1&titles=" + queryw + "&explaintext=1&formatversion=2&origin=*";
 Http.open("GET", url);
 Http.send();
 console.log("BlocksAI - running.")
 Http.onreadystatechange = (e) => {
-  console.log(Http.responseText)
+  var wikipediaresponse = Http.responseText;
+  console.log(wikipediaresponse);
+  var split1 = wikipediaresponse.split('<span class="s2">&quot;extract&quot;</span><span class="o">:</span> <span class="s2">&quot;', 2);
+  console.log(split1);
+  var split2 = split1[1].split('&quot;', 1);
+  console.log(split2);
+  document.getElementById("blockscontent").innerHTML = split2;
+  document.getElementById("blockszone").setAttribute("style", "display: block; font-family: 'stella.typeface', sans-serif; padding-left: 30px; z-index: 3; position: relative; margin: 0; padding-top: 0; padding-bottom: 0; background-color: #ededed;  margin-bottom: -7px; width: 100%;")
 }
 }
