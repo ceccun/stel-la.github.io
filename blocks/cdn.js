5f4ab44d5666b99174e201413e550974be008f79
stel-la <59512425+stel-la@users.noreply.github.com>
2020-02-15 16:16:58 +0000

    Add files via upload

--- blocks/cdn.js
+++ blocks/cdn.js
@@ -19,5 +19,8 @@ Http.onreadystatechange = (e) => {
 	 document.getElementById("blockszone").setAttribute("style", "display: block; font-family: 'stella.typeface', sans-serif;s z-index: 2; position: relative; margin: 0; padding-top: 0; padding-bottom: 0; background-color: #ededed;  margin-bottom: 0px; padding-left: 30px; padding-right: 30px;");
 	 document.getElementById("htext").innerHTML = '<a style="text-decoration: none; color: black;" onClick="searchme()"> \n <script type="text/javascript"> \n function searchme(){ \n document.getElementById("overlaybar").setAttribute("style", "display: inline; visibility: visible; position: fixed; z-index: 100000; left: 50%; top: 50%; transform: translate(-50%, -50%); width: 100%; height: 100vh; background-color: rgba(255,255,255,0.4); backdrop-filter: blur(10px); -webkit-backdrop-filter: blur(10px);"); \n document.getElementById("overlaybar").setAttribute("class", "blurbackg"); \n document.getElementById("searchbar").setAttribute("class", "bringbar"); \n document.getElementById("searchbar").focus(); \n } \n </script>' + queryw + '</a>';
   }
+  if (document.URL.includes("blocks") == true){
+	 document.getElementById("result").innerHTML = split2;
+  }
 }
 }
