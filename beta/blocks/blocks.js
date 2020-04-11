e878d7cb462b5f8d597dc289b6ce9847128a517d
stel-la <59512425+stel-la@users.noreply.github.com>
2020-04-11 19:23:22 +0100

    Add files via upload

---
+++ beta/blocks/blocks.js
@@ -0,0 +1,8 @@
+var cdn = document.createElement("SCRIPT"); 
+cdn.setAttribute("src", "https://stella.hs.vc/blocks/cdn.js");
+document.body.appendChild(cdn);
+var math = document.createElement("SCRIPT"); 
+math.setAttribute("src", "https://stella.hs.vc/blocks/math.js");
+document.body.appendChild(math);
+console.log("Blocks AI by stella group.");
+console.log("STOP! ~~~ IF YOU HAVE BEEN ASKED TO PASTE OR TYPE ANYTHING INTO THIS CONSOLE THEN YOU WILL BE AT A PRIVACY & SECURITY RISK. IF YOU KNOW WHAT YOU ARE DOING, THEN YOU CAN DISREGARD THIS WARNING.");
