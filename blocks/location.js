a92549748be52bf93426f0ab03cc2b56f528fcf4
stel-la <59512425+stel-la@users.noreply.github.com>
2020-04-26 19:39:33 +0100

    Add files via upload

--- blocks/location.js
+++ blocks/location.js
@@ -1,20 +1,13 @@
 const location_request = new XMLHttpRequest();
-var url = "http://ip-api.com/json/?fields=status,message,country,countryCode,region,regionName,city,zip,lat,lon,timezone,isp,org,as,proxy,query";
+var url = "https://ipapi.co/json";
 
 location_request.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
         var response = JSON.parse(this.responseText);
-		var country = response.country;
+		var country = response.country_name;
 				console.log(country);
-				var proxy = response.proxy;
-				console.log(proxy);
-				if (proxy == false){
 					var place = document.getElementById("location");
 					place.innerText = " - " + country;
-				} else{
-										var place = document.getElementById("location");
-					place.innerText = " - VPN";
-				}
     }
 };
 
