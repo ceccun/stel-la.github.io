d4072c4762808d7d85f0fe2ba1079f3bbf3b7323
Ejaz <73548455+ejaz4@users.noreply.github.com>
2021-02-22 12:47:10 +0000

    Update cdn.js

--- blocks/cdn.js
+++ blocks/cdn.js
@@ -59,6 +59,10 @@ function checkBlock(queryw){
 			placement(result, "fact", newquestion);
 			return;
 		}
+		if (newquestion == " ihfaz" || newquestion == "ihfaz"){
+			var result = "Pussymagnet69"
+			placement("ihfaz_rm", "fact", result)
+		}
 		if (newquestion == " eid" || newquestion == "eid"){
 					var result = "Eid al-Fitr 2020 begins at night on Saturday 23rd May.";
 			placement(result, "fact", newquestion);
